aaaf45433a384e27d01eb6fe00ce79493014634f
Eric Huang <eric.haoran.huang@gmail.com>
2025-10-20 13:37:39 -0400

    Finish cell-type specific training, moving onto hyperparameter tuning

--- run_script.sh
+++ run_script.sh
@@ -4,6 +4,7 @@
 # nohup bash ../discord.sh PYTHONPATH=".:$PYTHONPATH" python benchmark/test_continuous_tps.py -v -d herring --hvgs --per_cell_type -s remove_recovery --normalize &
 # nohup bash ../discord.sh PYTHONPATH=".:$PYTHONPATH" python benchmark/test_continuous_tps.py -v -d herring_gaba --hvgs --per_cell_type -s three_interpolation --normalize &
 # nohup bash ../discord.sh PYTHONPATH=".:$PYTHONPATH" python benchmark/test_dataset.py -v -d herring_gaba --hvgs --per_cell_type -s three_interpolation &
-nohup bash ../discord.sh PYTHONPATH=".:$PYTHONPATH" python benchmark/train_per_cell_type.py -v -d herring --hvgs --per_cell_type -s remove_recovery --normalize --cell_type_to_train SST &
+# nohup bash ../discord.sh PYTHONPATH=".:$PYTHONPATH" python benchmark/train_per_cell_type.py -v -d herring --hvgs --per_cell_type -s remove_recovery --normalize --cell_type_to_train OPC &
+nohup bash ../discord.sh PYTHONPATH=".:$PYTHONPATH" python benchmark/train_per_cell_type.py -v -d herring --hvgs --per_cell_type -s remove_recovery --normalize --cell_type_to_train L2-3_CUX2 &
 # PYTHONPATH=".:$PYTHONPATH" python benchmark/train_per_cell_type.py -v -d herring --hvgs --per_cell_type -s remove_recovery --cell_type_to_train MGE_dev
 # PYTHONPATH=".:$PYTHONPATH" python benchmark/train_per_cell_type.py -v -d herring --hvgs --per_cell_type -s remove_recovery --normalize --cell_type_to_train MGE_dev
